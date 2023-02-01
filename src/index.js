37dbe1b826e3285348ed42890aae030ce59a1ce6
KMJ <aaeexx@naver.com>
2023-02-01 22:34:40 +0900

    23.02.01

--- src/index.js
+++ src/index.js
@@ -4,11 +4,27 @@ const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
-const reducer = (state = 1) => {
+const reducer = (state = 1,action) => {
+    if(action.type == 'add'){
+        return(
+            state + 1
+        )
+    }else if(action.type == 'minus'){
+        return(
+            state - 1
+        )
+    }
     return state;
 }
 
 const store = createStore(reducer);
+
+store.dispatch({type:'add'});
+store.dispatch({type:'add'});
+store.dispatch({type:'add'});
+store.dispatch({type:'minus'});
+store.dispatch({type:''});
+
 
 console.log(store.getState());
 
