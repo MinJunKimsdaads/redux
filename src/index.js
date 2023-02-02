62a00f689e7e858fe1b2c0a3ad37e7ad1b425097
KMJ <aaeexx@naver.com>
2023-02-02 21:34:54 +0900

    20230202

--- src/index.js
+++ src/index.js
@@ -1,30 +1,81 @@
 import { createStore } from "redux";
 
-const add = document.getElementById('add');
-const minus = document.getElementById('minus');
-const number = document.querySelector('span');
+// const add = document.getElementById('add');
+// const minus = document.getElementById('minus');
+// const number = document.querySelector('span');
 
-const reducer = (state = 1,action) => {
-    if(action.type == 'add'){
-        return(
-            state + 1
-        )
-    }else if(action.type == 'minus'){
-        return(
-            state - 1
-        )
+// const add1 = 'add';
+// const minus1 = 'minus';
+
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const ul = document.querySelector('ul');
+
+
+
+const ADD_TODO =  'ADD_TODO';
+const DELETE_TODO = 'DELETE_TODO';
+
+const reducer = (state=[],action) => {
+    // if(action.type == 'add'){
+    //     return(
+    //         state + 1
+    //     )
+    // }else if(action.type == 'minus'){
+    //     return(
+    //         state - 1
+    //     )
+    // }else{
+    //     return state;
+    // }
+    
+    // switch(action.type){
+    //     case 'add':
+    //         return state + 1
+    //     case 'minus':
+    //         return state - 1
+    //     default:
+    //         return state
+    // }
+
+    console.log(action);
+
+    switch(action.type){
+        case ADD_TODO:
+            return []
+        case DELETE_TODO:
+            return []
+        default :
+            return []
     }
-    return state;
 }
+
 
 const store = createStore(reducer);
 
-store.dispatch({type:'add'});
-store.dispatch({type:'add'});
-store.dispatch({type:'add'});
-store.dispatch({type:'minus'});
-store.dispatch({type:''});
+const onSubmit = e => {
+    e.preventDefault();
+    const toDo = input.value;
+    input.value = "";
+    store.dispatch({type:ADD_TODO,context:toDo});
+}
+
+form.addEventListener('submit',onSubmit);
 
 
-console.log(store.getState());
+// const handleAdd = () => {
+//     store.dispatch({type:add1});
+// }
+
+// const handleMinus = () => {
+//     store.dispatch({type:minus1});
+// }
+
+// add.addEventListener('click',handleAdd);
+// minus.addEventListener('click',handleMinus);
+
+// const onChange = () => {
+//     number.innerText = store.getState();
+// }
+// store.subscribe(onChange);
 
